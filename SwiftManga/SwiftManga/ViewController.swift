354e6f3d1426e2fa32d9751871786584d7495bd3
TakehiroTakahashi <taka.mc.0509@gmail.com>
2018-11-21 07:29:24 +0900

    プロジェクトの新規作成

---
+++ SwiftManga/SwiftManga/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftManga
+//
+//  Created by 高橋岳宏 on 2018/11/21.
+//  Copyright © 2018 高橋岳宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
